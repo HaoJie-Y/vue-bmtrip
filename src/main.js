48547d0265d26b6aecabe1599ae5d50bf508dbc8
Hannah <hannah-yang@outlook.com>
2019-11-12 23:30:45 +0800

    1111

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router"
 import VueTouch from "vue-touch"
 import Vuex from "vuex"
 import "@common/components"
-
+//
 Vue.config.productionTip = false
 Vue.use(VueTouch);
 Vue.use(Vuex);
